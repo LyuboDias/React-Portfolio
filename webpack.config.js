d638eaac1957b0aec1f1e2bdc495e65fda7ec10d
Lyubomir Dias <lyubomirdias@gmail.com>
2022-03-11 17:24:09 +0000

    testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                    "babel-loader",
-                ],
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(gif|svg|jpg|png)$/,
-                loader: "file-loader",
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    mode: 'production' // | 'development' | 'none'
-};
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//         path: __dirname + '/dist',
+//         publicPath: '/',
+//         filename: 'bundle.js'
+//     },
+//     devServer: {
+//         contentBase: './dist',
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.(js|jsx)$/,
+//                 exclude: /node_modules/,
+//                 use: [
+//                     "babel-loader",
+//                 ],
+//             },
+//             {
+//                 test: /\.scss$/,
+//                 use: [
+//                     'style-loader',
+//                     'css-loader',
+//                     // 'sass-loader',
+//                 ],
+//             },
+//             {
+//                 test: /\.(gif|svg|jpg|png)$/,
+//                 loader: "file-loader",
+//             },
+//         ]
+//     },
+//     resolve: {
+//         extensions: ['.js', '.jsx']
+//     },
+//     mode: 'production' // | 'development' | 'none'
+// };
